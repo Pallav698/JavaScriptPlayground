0b2dd4d778f200485adc9e0b150d0eeb3364c4b0
Pallav <iampallav698@gmail.com>
2024-01-08 19:01:06 +0530

    bulb commit

---
+++ Project1/index.js
@@ -0,0 +1,15 @@
+let btn = document.getElementById('btn');
+let bulb = document.getElementById('img');
+
+btn.addEventListener('click', handleClick);
+
+function handleClick() {
+    if(btn.innerText === 'Turn On'){
+        btn.innerText = 'Turn Off';
+        bulb.src = 'Resources/Off.jpg';
+    }
+    else{
+        btn.innerText = 'Turn On';
+        bulb.src = "Resources/On.avif";
+    }
+}
